2d4efa299f9a48305c8782dcee1f2a7d5f28673d
deming16 <deminglindm@gmail.com>
2019-01-19 17:47:39 +0800

    Get ball to bounce

---
+++ New Year Pong/Assets/Pong/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+
+    void Start()
+    {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+    }
+
+
+}
